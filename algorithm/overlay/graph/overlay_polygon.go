125b5bd4cf3704ed1db519b46941af5644bbc128
yueke <yuekewin@gmail.com>
2021-12-02 18:54:02 +0800

    perf: algorithm-overlay添加graph目录

--- algorithm/overlay/graph/overlay_polygon.go
+++ algorithm/overlay/graph/overlay_polygon.go
@@ -1,18 +1,17 @@
-package overlay
+package graph
 
 import (
 	"github.com/spatial-go/geoos/algorithm"
-	"github.com/spatial-go/geoos/algorithm/calc"
-	"github.com/spatial-go/geoos/algorithm/filter"
 	"github.com/spatial-go/geoos/algorithm/matrix"
 	"github.com/spatial-go/geoos/algorithm/matrix/envelope"
+	"github.com/spatial-go/geoos/algorithm/overlay"
 	"github.com/spatial-go/geoos/algorithm/relate"
 )
 
 // PolygonOverlay  Computes the overlay of two geometries,either or both of which may be nil.
 type PolygonOverlay struct {
-	*PointOverlay
-	subjectPlane, clippingPlane *Plane
+	*overlay.PointOverlay
+	subjectPlane, clippingPlane *overlay.Plane
 }
 
 // isPolyInHole
@@ -57,9 +56,9 @@ func (p *PolygonOverlay) isPolyInHole(polyMatrix matrix.PolygonMatrix) (inHole b
 
 // Union  Computes the Union of two geometries,either or both of which may be nil.
 func (p *PolygonOverlay) Union() (matrix.Steric, error) {
-	if res, ok := p.unionCheck(); !ok {
-		return res, nil
-	}
+	//if res, ok := p.unionCheck(); !ok {
+	//	return res, nil
+	//}
 	if ps, ok := p.Subject.(matrix.PolygonMatrix); ok {
 		if pc, ok := p.Clipping.(matrix.PolygonMatrix); ok {
 			inter := envelope.Bound(ps.Bound()).IsIntersects(envelope.Bound(pc.Bound()))
@@ -76,13 +75,14 @@ func (p *PolygonOverlay) Union() (matrix.Steric, error) {
 			if mark, _ := im.Matches("FF**0****"); mark {
 				return matrix.Collection{p.Subject.(matrix.PolygonMatrix), p.Clipping.(matrix.PolygonMatrix)}, nil
 			}
-
-			cpo := &ComputeMergeOverlay{p}
-
-			cpo.prepare()
-			_, exitingPoints := cpo.Weiler()
-
-			result := ToPolygonMatrix(cpo.ComputePolygon(exitingPoints, cpo))
+			//
+			//cpo := &ComputeMergeOverlay{p}
+			//
+			//cpo.prepare()
+			//_, exitingPoints := cpo.Weiler()
+			//
+			//result := ToPolygonMatrix(cpo.ComputePolygon(exitingPoints, cpo))
+			var result matrix.Steric
 			return result, nil
 		}
 	}
@@ -91,9 +91,9 @@ func (p *PolygonOverlay) Union() (matrix.Steric, error) {
 
 // Intersection  Computes the Intersection of two geometries,either or both of which may be nil.
 func (p *PolygonOverlay) Intersection() (matrix.Steric, error) {
-	if res, ok := p.intersectionCheck(); !ok {
-		return res, nil
-	}
+	//if res, ok := p.intersectionCheck(); !ok {
+	//	return res, nil
+	//}
 	var poly matrix.PolygonMatrix
 	if p, ok := p.Subject.(matrix.PolygonMatrix); ok {
 		poly = p
@@ -101,20 +101,20 @@ func (p *PolygonOverlay) Intersection() (matrix.Steric, error) {
 		return nil, algorithm.ErrNotMatchType
 	}
 	switch c := p.Clipping.(type) {
-	case matrix.Matrix:
-
-		inter := envelope.Bound(poly.Bound()).IsIntersects(envelope.Bound(c.Bound()))
-		if mark := relate.IM(poly, c, inter).IsContains(); mark {
-			return c, nil
-		}
-		return nil, nil
-	case matrix.LineMatrix:
-		result := matrix.Collection{}
-		for _, ring := range poly {
-			res := intersectLine(ring, c)
-			result = append(result, res...)
-		}
-		return LineMerge(result), nil
+	//case matrix.Matrix:
+	//
+	//	inter := envelope.Bound(poly.Bound()).IsIntersects(envelope.Bound(c.Bound()))
+	//	if mark := relate.IM(poly, c, inter).IsContains(); mark {
+	//		return c, nil
+	//	}
+	//	return nil, nil
+	//case matrix.LineMatrix:
+	//	result := matrix.Collection{}
+	//	for _, ring := range poly {
+	//		res := intersectLine(ring, c)
+	//		result = append(result, res...)
+	//	}
+	//	return LineMerge(result), nil
 	case matrix.PolygonMatrix:
 
 		inter := envelope.Bound(poly.Bound()).IsIntersects(envelope.Bound(c.Bound()))
@@ -126,11 +126,12 @@ func (p *PolygonOverlay) Intersection() (matrix.Steric, error) {
 			return poly, nil
 		}
 
-		cpo := &ComputeClipOverlay{p}
-
-		cpo.prepare()
-		_, exitingPoints := cpo.Weiler()
-		result := ToPolygonMatrix(cpo.ComputePolygon(exitingPoints, cpo))
+		//cpo := &ComputeClipOverlay{p}
+		//
+		//cpo.prepare()
+		//_, exitingPoints := cpo.Weiler()
+		//result := ToPolygonMatrix(cpo.ComputePolygon(exitingPoints, cpo))
+		var result  matrix.PolygonMatrix
 		// remove the parts of result in hole
 		intersectionResult := matrix.PolygonMatrix{}
 		for _, r := range result {
@@ -151,9 +152,9 @@ func (p *PolygonOverlay) Intersection() (matrix.Steric, error) {
 // One can think of this as GeometryA - Intersection(A,B).
 // If A is completely contained in B then an empty geometry collection is returned.
 func (p *PolygonOverlay) Difference() (matrix.Steric, error) {
-	if res, ok := p.differenceCheck(); !ok {
-		return res, nil
-	}
+	//if res, ok := p.differenceCheck(); !ok {
+	//	return res, nil
+	//}
 	if poly, ok := p.Subject.(matrix.PolygonMatrix); ok {
 		if c, ok := p.Clipping.(matrix.PolygonMatrix); ok {
 
@@ -168,11 +169,12 @@ func (p *PolygonOverlay) Difference() (matrix.Steric, error) {
 				return poly, nil
 			}
 
-			cpo := &ComputeMainOverlay{p}
-
-			cpo.prepare()
-			_, exitingPoints := cpo.Weiler()
-			result := ToPolygonMatrix(cpo.ComputePolygon(exitingPoints, cpo))
+			//cpo := &ComputeMainOverlay{p}
+			//
+			//cpo.prepare()
+			//_, exitingPoints := cpo.Weiler()
+			//result := ToPolygonMatrix(cpo.ComputePolygon(exitingPoints, cpo))
+			var result matrix.PolygonMatrix
 			return result, nil
 		}
 	}
@@ -183,7 +185,7 @@ func (p *PolygonOverlay) Difference() (matrix.Steric, error) {
 // One can think of this as GeometryB - Intersection(A,B).
 // If B is completely contained in A then an empty geometry collection is returned.
 func (p *PolygonOverlay) DifferenceReverse() (matrix.Steric, error) {
-	newPoly := &PolygonOverlay{PointOverlay: &PointOverlay{Subject: p.Clipping, Clipping: p.Subject}}
+	newPoly := &PolygonOverlay{PointOverlay: &overlay.PointOverlay{Subject: p.Clipping, Clipping: p.Subject}}
 	return newPoly.Difference()
 }
 
@@ -202,158 +204,76 @@ func (p *PolygonOverlay) SymDifference() (matrix.Steric, error) {
 }
 
 // prepare prepare two polygonal geometries.
-func (p *PolygonOverlay) prepare() {
-	p.subjectPlane = &Plane{}
-	for _, v2 := range p.Subject.(matrix.PolygonMatrix) {
-		for i, v1 := range v2 {
-			if i < len(v2)-1 {
-				p.subjectPlane.AddPoint(&Vertex{Matrix: matrix.Matrix(v1)})
-			}
-		}
-		p.subjectPlane.CloseRing()
-		p.subjectPlane.Rank = calc.OverlayMain
-	}
-	p.clippingPlane = &Plane{}
-	for _, v2 := range p.Clipping.(matrix.PolygonMatrix) {
-		for i, v1 := range v2 {
-			if i < len(v2)-1 {
-				p.clippingPlane.AddPoint(&Vertex{Matrix: matrix.Matrix(v1)})
-			}
-		}
-		p.clippingPlane.CloseRing()
-		p.clippingPlane.Rank = calc.OverlayCut
-	}
-}
+//func (p *PolygonOverlay) prepare() {
+//	p.subjectPlane = &Plane{}
+//	for _, v2 := range p.Subject.(matrix.PolygonMatrix) {
+//		for i, v1 := range v2 {
+//			if i < len(v2)-1 {
+//				p.subjectPlane.AddPoint(&Vertex{Matrix: matrix.Matrix(v1)})
+//			}
+//		}
+//		p.subjectPlane.CloseRing()
+//		p.subjectPlane.Rank = calc.OverlayMain
+//	}
+//	p.clippingPlane = &Plane{}
+//	for _, v2 := range p.Clipping.(matrix.PolygonMatrix) {
+//		for i, v1 := range v2 {
+//			if i < len(v2)-1 {
+//				p.clippingPlane.AddPoint(&Vertex{Matrix: matrix.Matrix(v1)})
+//			}
+//		}
+//		p.clippingPlane.CloseRing()
+//		p.clippingPlane.Rank = calc.OverlayCut
+//	}
+//}
 
 // Weiler Weiler overlay.
-func (p *PolygonOverlay) Weiler() (enteringPoints, exitingPoints []Vertex) {
-
-	// TODO overlay ...
-	filtEntering := &UniqueVertexFilter{}
-	filtExiting := &UniqueVertexFilter{}
-	for _, v := range p.subjectPlane.Lines {
-		for _, vClip := range p.clippingPlane.Lines {
-
-			mark, ips :=
-				relate.Intersection(v.Start.Matrix, v.End.Matrix, vClip.Start.Matrix, vClip.End.Matrix)
-			for _, ip := range ips {
-				if ip.IsCollinear {
-					//continue //TODO
-				}
-				inV, _ := relate.InLineVertex(ip.Matrix, matrix.LineMatrix{v.Start.Matrix, v.End.Matrix})
-				inVClip, _ := relate.InLineVertex(ip.Matrix, matrix.LineMatrix{vClip.Start.Matrix, vClip.End.Matrix})
-				if inV && inVClip {
-					//continue //TODO
-				}
-				if !ip.IsIntersectionPoint {
-					continue
-				}
-				ipVer := &Vertex{}
-				ipVer.Matrix = ip.Matrix
-				ipVer.IsIntersectionPoint = ip.IsIntersectionPoint
-				ipVer.IsEntering = ip.IsEntering
-				if mark {
-					if ipVer.IsEntering && filtEntering.Filter(*ipVer) {
-						enteringPoints = append(enteringPoints, *ipVer)
-						AddPointToVertexSlice(p.subjectPlane.Rings, v.Start, v.End, ipVer)
-						AddPointToVertexSlice(p.clippingPlane.Rings, vClip.Start, vClip.End, ipVer)
-					}
-					if !ipVer.IsEntering && filtExiting.Filter(*ipVer) {
-						exitingPoints = append(exitingPoints, *ipVer)
-						AddPointToVertexSlice(p.subjectPlane.Rings, v.Start, v.End, ipVer)
-						AddPointToVertexSlice(p.clippingPlane.Rings, vClip.Start, vClip.End, ipVer)
-					}
-
-				}
-			}
-		}
-	}
-
-	return
-}
+//func (p *PolygonOverlay) Weiler() (enteringPoints, exitingPoints []Vertex) {
+//
+//	// TODO overlay ...
+//	filtEntering := &UniqueVertexFilter{}
+//	filtExiting := &UniqueVertexFilter{}
+//	for _, v := range p.subjectPlane.Lines {
+//		for _, vClip := range p.clippingPlane.Lines {
+//
+//			mark, ips :=
+//				relate.Intersection(v.Start.Matrix, v.End.Matrix, vClip.Start.Matrix, vClip.End.Matrix)
+//			for _, ip := range ips {
+//				if ip.IsCollinear {
+//					//continue //TODO
+//				}
+//				inV, _ := relate.InLineVertex(ip.Matrix, matrix.LineMatrix{v.Start.Matrix, v.End.Matrix})
+//				inVClip, _ := relate.InLineVertex(ip.Matrix, matrix.LineMatrix{vClip.Start.Matrix, vClip.End.Matrix})
+//				if inV && inVClip {
+//					//continue //TODO
+//				}
+//				if !ip.IsIntersectionPoint {
+//					continue
+//				}
+//				ipVer := &Vertex{}
+//				ipVer.Matrix = ip.Matrix
+//				ipVer.IsIntersectionPoint = ip.IsIntersectionPoint
+//				ipVer.IsEntering = ip.IsEntering
+//				if mark {
+//					if ipVer.IsEntering && filtEntering.Filter(*ipVer) {
+//						enteringPoints = append(enteringPoints, *ipVer)
+//						AddPointToVertexSlice(p.subjectPlane.Rings, v.Start, v.End, ipVer)
+//						AddPointToVertexSlice(p.clippingPlane.Rings, vClip.Start, vClip.End, ipVer)
+//					}
+//					if !ipVer.IsEntering && filtExiting.Filter(*ipVer) {
+//						exitingPoints = append(exitingPoints, *ipVer)
+//						AddPointToVertexSlice(p.subjectPlane.Rings, v.Start, v.End, ipVer)
+//						AddPointToVertexSlice(p.clippingPlane.Rings, vClip.Start, vClip.End, ipVer)
+//					}
+//
+//				}
+//			}
+//		}
+//	}
+//
+//	return
+//}
 
 // UniqueVertexFilter  A Filter that extracts a unique array.
-type UniqueVertexFilter struct {
-	Ips []Vertex
-}
 
-// Filter Performs an operation with the provided .
-func (u *UniqueVertexFilter) Filter(ip interface{}) bool {
-	return u.add(ip)
-}
 
-// Entities  Returns the gathered Matrixes.
-func (u *UniqueVertexFilter) Entities() interface{} {
-	return u.Ips
-}
-
-func (u *UniqueVertexFilter) add(ip interface{}) bool {
-	hasMatrix := false
-	for _, v := range u.Ips {
-		if v.Matrix.Equals(ip.(Vertex).Matrix) {
-			hasMatrix = true
-			break
-		}
-	}
-	if !hasMatrix {
-		u.Ips = append(u.Ips, ip.(Vertex))
-		return true
-	}
-	return false
-}
-
-// compile time checks
-var (
-	_ filter.Filter = &UniqueVertexFilter{}
-)
-
-// ComputePolygon compute overlay.
-func (p *PolygonOverlay) ComputePolygon(exitingPoints []Vertex, cpo ComputePolyOverlay) *Plane {
-	var pol = &Plane{}
-	for _, iterPoint := range exitingPoints {
-		if iterPoint.IsChecked {
-			continue
-		}
-		edge := &Edge{}
-		pol.Edge = edge
-		pol.Rings = append(pol.Rings, edge)
-
-		start := &iterPoint
-		next := &Vertex{Matrix: matrix.Matrix{start.X(), start.Y()}}
-		start.IsChecked = true
-
-		for {
-			next = cpo.Next(pol, next)
-			if where, err := SliceContains(exitingPoints, next); err == nil {
-				exitingPoints[where].IsChecked = true
-			}
-			if next.X() == start.X() && next.Y() == start.Y() {
-				pol.CloseRing()
-				break
-			}
-		}
-	}
-
-	return pol
-}
-
-// ToPolygonMatrix ...
-func ToPolygonMatrix(poly *Plane) matrix.PolygonMatrix {
-	result := matrix.PolygonMatrix{}
-	for _, v2 := range poly.Rings {
-		if len(v2.Vertexes) < 3 {
-			// skip invalid polygon
-			continue
-		}
-		var edge matrix.LineMatrix
-		for _, v1 := range v2.Vertexes {
-			edge = append(edge, v1.Matrix)
-		}
-		if !matrix.Matrix(edge[len(edge)-1]).Equals(matrix.Matrix(edge[0])) {
-			edge = append(edge, edge[0])
-		}
-		result = append(result, edge)
-	}
-
-	return result
-}
---
+++ algorithm/overlay/graph/overlay_polygon_test.go
@@ -0,0 +1,325 @@
+package graph
+
+import (
+	"github.com/spatial-go/geoos/algorithm/overlay"
+	"reflect"
+	"testing"
+
+	"github.com/spatial-go/geoos"
+	"github.com/spatial-go/geoos/algorithm/matrix"
+	"github.com/spatial-go/geoos/algorithm/measure"
+)
+
+func TestPolygonOverlay_Intersection(t *testing.T) {
+	type fields struct {
+		PointOverlay  *overlay.PointOverlay
+		subjectPlane  *overlay.Plane
+		clippingPlane *overlay.Plane
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    matrix.Steric
+		wantErr bool
+	}{
+		{"poly point0", fields{PointOverlay: &overlay.PointOverlay{matrix.PolygonMatrix{{{90, 90}, {90, 101}, {101, 101}, {101, 90}, {90, 90}}}, matrix.Matrix{100, 100}}}, matrix.Matrix{100, 100}, false},
+		{"poly line0", fields{PointOverlay: &overlay.PointOverlay{matrix.PolygonMatrix{{{90, 90}, {90, 101}, {101, 101}, {101, 90}, {90, 90}}},
+			matrix.LineMatrix{{100, 100}, {101, 101}}}},
+			matrix.Collection{matrix.Matrix{101, 101}}, false},
+		{"poly line1", fields{PointOverlay: &overlay.PointOverlay{matrix.PolygonMatrix{{{105, 105}, {105, 101}, {101, 101}, {101, 105}, {105, 105}}},
+			matrix.LineMatrix{{100, 100}, {90, 101}}}},
+			matrix.Collection{}, false},
+
+		{"poly poly1", fields{PointOverlay: &overlay.PointOverlay{matrix.PolygonMatrix{{{100, 100}, {100, 101}, {101, 101}, {101, 100}, {100, 100}}},
+			matrix.PolygonMatrix{{{90, 90}, {90, 101}, {101, 101}, {101, 90}, {90, 90}}},
+		}},
+			matrix.PolygonMatrix{{{101, 100}, {100, 100}, {100, 101}, {101, 101}, {101, 100}}}, false},
+
+		{"poly poly2", fields{PointOverlay: &overlay.PointOverlay{matrix.PolygonMatrix{{{105, 105}, {105, 103}, {103, 103}, {103, 105}, {105, 105}}},
+			matrix.PolygonMatrix{{{100, 100}, {100, 101}, {101, 101}, {101, 100}, {100, 100}}},
+		}},
+			matrix.PolygonMatrix{}, false},
+		{"poly poly3", fields{PointOverlay: &overlay.PointOverlay{matrix.PolygonMatrix{{{0, 0}, {10, 0}, {10, 10}, {0, 10}, {0, 0}}},
+			matrix.PolygonMatrix{{{5, 5}, {15, 5}, {15, 15}, {5, 15}, {5, 5}}},
+		}},
+			matrix.PolygonMatrix{{{5, 10}, {10, 10}, {10, 5}, {5, 5}, {5, 10}}}, false},
+		{"poly poly3-1", fields{PointOverlay: &overlay.PointOverlay{matrix.PolygonMatrix{{{0, 0}, {10, 0}, {10, 10}, {0, 10}, {0, 0}}},
+			matrix.PolygonMatrix{{{5, 5}, {5, 15}, {15, 15}, {15, 5}, {5, 5}}},
+		}},
+			matrix.PolygonMatrix{{{5, 10}, {10, 10}, {10, 5}, {5, 5}, {5, 10}}}, false},
+		{"poly poly4", fields{PointOverlay: &overlay.PointOverlay{
+			matrix.PolygonMatrix{{{111.30523681640625, 38.11727165830543}, {112.34344482421875, 38.11727165830543}, {112.34344482421875, 38.89103282648846},
+				{111.30523681640625, 38.89103282648846}, {111.30523681640625, 38.117271658305}}},
+			matrix.PolygonMatrix{{{111.50848388671875, 37.6359849542696}, {112.64007568359375, 37.6359849542696}, {112.64007568359375, 38.35027253825765},
+				{111.50848388671875, 38.35027253825765}, {111.50848388671875, 37.6359849542696}}},
+		}},
+			matrix.PolygonMatrix{{{112.34344482421875, 38.35027253825765}, {112.34344482421875, 38.11727165830543}, {111.50848388671875, 38.11727165830543},
+				{111.50848388671875, 38.35027253825765}, {112.34344482421875, 38.35027253825765}}}, false},
+
+		{"poly poly5", fields{PointOverlay: &overlay.PointOverlay{matrix.PolygonMatrix{{{0, 0}, {10, 0}, {10, 10}, {0, 10}, {0, 0}}, {{1, 1}, {9, 1}, {9, 9}, {1, 9}, {1, 1}}},
+			matrix.PolygonMatrix{{{5, 5}, {15, 5}, {15, 15}, {5, 15}, {5, 5}}},
+		}},
+			matrix.PolygonMatrix{{{9, 9}, {9, 5}, {10, 5}, {10, 10}, {5, 10}, {5, 9}, {9, 9}}}, false},
+		{"poly poly5-1", fields{PointOverlay: &overlay.PointOverlay{matrix.PolygonMatrix{{{5, 5}, {15, 5}, {15, 15}, {5, 15}, {5, 5}}},
+			matrix.PolygonMatrix{{{0, 0}, {10, 0}, {10, 10}, {0, 10}, {0, 0}}, {{1, 1}, {9, 1}, {9, 9}, {1, 9}, {1, 1}}},
+		}},
+			matrix.PolygonMatrix{{{9, 9}, {9, 5}, {10, 5}, {10, 10}, {5, 10}, {5, 9}, {9, 9}}}, false},
+	}
+	for _, tt := range tests {
+		if !geoos.GeoosTestTag && tt.name != "poly poly5" {
+			continue
+		}
+		t.Run(tt.name, func(t *testing.T) {
+			p := &PolygonOverlay{
+				PointOverlay:  tt.fields.PointOverlay,
+				subjectPlane:  tt.fields.subjectPlane,
+				clippingPlane: tt.fields.clippingPlane,
+			}
+			got, err := p.Intersection()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("PolygonOverlay.Intersection() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !got.Equals(tt.want) {
+				if gotPoly, ok := got.(matrix.PolygonMatrix); ok {
+					if wantPoly, ok := tt.want.(matrix.PolygonMatrix); ok {
+						if measure.AreaOfPolygon(gotPoly) == measure.AreaOfPolygon(wantPoly) {
+							return
+						}
+					}
+				}
+				t.Errorf("PolygonOverlay.Intersection()%v = %v, \nwant %v type %T, want %T", tt.name, got, tt.want, got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPolygonOverlay_Union(t *testing.T) {
+	type fields struct {
+		PointOverlay  *overlay.PointOverlay
+		subjectPlane  *overlay.Plane
+		clippingPlane *overlay.Plane
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    []matrix.Steric
+		wantErr bool
+	}{
+		{"poly poly", fields{PointOverlay: &overlay.PointOverlay{matrix.PolygonMatrix{{{0, 0}, {10, 0}, {10, 10}, {0, 10}, {0, 0}}},
+			matrix.PolygonMatrix{{{5, 5}, {15, 5}, {15, 15}, {5, 15}, {5, 5}}},
+		}},
+			[]matrix.Steric{matrix.PolygonMatrix{{{5, 10}, {0, 10}, {0, 0}, {10, 0}, {10, 5}, {15, 5}, {15, 15}, {5, 15}, {5, 10}}}}, false},
+
+		{"poly poly01", fields{PointOverlay: &overlay.PointOverlay{matrix.PolygonMatrix{{{0, 0}, {10, 0}, {10, 10}, {0, 10}, {0, 0}}},
+			matrix.PolygonMatrix{{{5, 5}, {5, 15}, {15, 15}, {15, 5}, {5, 5}}},
+		}},
+			[]matrix.Steric{
+				matrix.PolygonMatrix{{{10, 5}, {10, 0}, {0, 0}, {0, 10}, {5, 10}, {5, 15}, {15, 15}, {15, 5}, {10, 5}}},
+				matrix.PolygonMatrix{{{5, 10}, {0, 10}, {0, 0}, {10, 0}, {10, 5}, {15, 5}, {15, 15}, {5, 15}, {5, 10}}},
+			}, false},
+
+		{"poly poly02", fields{PointOverlay: &overlay.PointOverlay{matrix.PolygonMatrix{{{0, 0}, {10, 0}, {10, 10}, {0, 10}, {0, 0}}, {{1, 1}, {9, 1}, {9, 9}, {1, 9}, {1, 1}}},
+			matrix.PolygonMatrix{{{5, 5}, {15, 5}, {15, 15}, {5, 15}, {5, 5}}},
+		}},
+			[]matrix.Steric{matrix.PolygonMatrix{{{5, 10}, {0, 10}, {0, 0}, {10, 0}, {10, 5}, {15, 5}, {15, 15}, {5, 15}, {5, 10}}, {{5, 9}, {1, 9}, {1, 1}, {9, 1}, {9, 5}, {5, 5}, {5, 9}}}}, false},
+
+		{"poly poly03", fields{PointOverlay: &overlay.PointOverlay{matrix.PolygonMatrix{{{5, 5}, {15, 5}, {15, 15}, {5, 15}, {5, 5}}},
+			matrix.PolygonMatrix{{{0, 0}, {10, 0}, {10, 10}, {0, 10}, {0, 0}}, {{1, 1}, {9, 1}, {9, 9}, {1, 9}, {1, 1}}},
+		}},
+			[]matrix.Steric{matrix.PolygonMatrix{{{10, 5}, {15, 5}, {15, 15}, {5, 15}, {5, 10}, {0, 10}, {0, 0}, {10, 0}, {10, 5}}, {{9, 5}, {5, 5}, {5, 9}, {1, 9}, {1, 1}, {9, 1}, {9, 5}}}}, false},
+
+		{"poly poly1", fields{PointOverlay: &overlay.PointOverlay{matrix.PolygonMatrix{{{100, 100}, {100, 101}, {101, 101}, {101, 100}, {100, 100}}},
+			matrix.PolygonMatrix{{{90, 90}, {90, 101}, {101, 101}, {101, 90}, {90, 90}}},
+		}},
+			[]matrix.Steric{matrix.PolygonMatrix{{{90, 90}, {90, 101}, {101, 101}, {101, 90}, {90, 90}}}}, false},
+
+		{name: "poly 1",
+			fields: fields{PointOverlay: &overlay.PointOverlay{matrix.PolygonMatrix{{{1, 1}, {2, 1}, {2, 2}, {1, 2}, {1, 1}}},
+				matrix.PolygonMatrix{{{3, 1}, {5, 1}, {5, 2}, {3, 2}, {3, 1}}},
+			},
+			}, want: []matrix.Steric{matrix.Collection{matrix.PolygonMatrix{{{1, 1}, {2, 1}, {2, 2}, {1, 2}, {1, 1}}},
+				matrix.PolygonMatrix{{{3, 1}, {5, 1}, {5, 2}, {3, 2}, {3, 1}}}},
+				matrix.Collection{matrix.PolygonMatrix{{{1, 1}, {2, 1}, {2, 2}, {1, 2}, {1, 1}}},
+					matrix.PolygonMatrix{{{3, 1}, {5, 1}, {5, 2}, {3, 2}, {3, 1}}}},
+			},
+			wantErr: false},
+
+		{name: "poly 2",
+			fields: fields{PointOverlay: &overlay.PointOverlay{matrix.PolygonMatrix{{{1, 1}, {2, 1}, {2, 2}, {1, 2}, {1, 1}}},
+				matrix.PolygonMatrix{{{2, 1}, {5, 1}, {5, 2}, {2, 2}, {2, 1}}},
+			},
+			}, want: []matrix.Steric{matrix.PolygonMatrix{{{1, 1}, {5, 1}, {5, 2}, {1, 2}, {1, 1}}},
+				matrix.PolygonMatrix{{{2, 2}, {1, 2}, {1, 1}, {2, 1}, {5, 1}, {5, 2}, {2, 2}}},
+			},
+			wantErr: false},
+
+		{name: "poly 3",
+			fields: fields{PointOverlay: &overlay.PointOverlay{matrix.PolygonMatrix{{{1, 1}, {2, 1}, {2, 2}, {1, 2}, {1, 1}}},
+				matrix.PolygonMatrix{{{2, 2}, {5, 2}, {5, 3}, {2, 3}, {2, 2}}},
+			},
+			}, want: []matrix.Steric{matrix.Collection{matrix.PolygonMatrix{{{2, 2}, {2, 1}, {1, 1}, {1, 2}, {2, 2}}},
+				matrix.PolygonMatrix{{{2, 2}, {2, 3}, {5, 3}, {5, 2}, {2, 2}}}},
+				matrix.Collection{matrix.PolygonMatrix{{{1, 1}, {2, 1}, {2, 2}, {1, 2}, {1, 1}}},
+					matrix.PolygonMatrix{{{2, 2}, {5, 2}, {5, 3}, {2, 3}, {2, 2}}}},
+			},
+			wantErr: false},
+
+		{name: "poly 4",
+			fields: fields{PointOverlay: &overlay.PointOverlay{matrix.PolygonMatrix{{{1, 2}, {3, 2}, {3, 3}, {1, 3}, {1, 2}}},
+				matrix.PolygonMatrix{{{2, 1}, {5, 1}, {5, 5}, {2, 5}, {2, 1}}},
+			},
+			}, want: []matrix.Steric{matrix.PolygonMatrix{{{2, 2}, {1, 2}, {1, 3}, {2, 3}, {2, 5}, {5, 5}, {5, 1}, {2, 1}, {2, 2}}},
+				matrix.PolygonMatrix{{{2, 3}, {1, 3}, {1, 2}, {2, 2}, {2, 1}, {5, 1}, {5, 5}, {2, 5}, {2, 3}}},
+			},
+			wantErr: false},
+
+		{name: "poly 5",
+			fields: fields{PointOverlay: &overlay.PointOverlay{matrix.PolygonMatrix{{{1, 1}, {5, 1}, {5, 5}, {1, 5}, {1, 1}}},
+				matrix.PolygonMatrix{{{2, 2}, {3, 2}, {3, 3}, {2, 3}, {2, 2}}},
+			},
+			}, want: []matrix.Steric{matrix.PolygonMatrix{{{1, 1}, {1, 5}, {5, 5}, {5, 1}, {1, 1}}},
+				matrix.PolygonMatrix{{{1, 1}, {5, 1}, {5, 5}, {1, 5}, {1, 1}}},
+			},
+			wantErr: false},
+
+		{name: "poly 6",
+			fields: fields{PointOverlay: &overlay.PointOverlay{matrix.PolygonMatrix{{{1, 1}, {2, 1}, {2, 2}, {1, 2}, {1, 1}}},
+				matrix.PolygonMatrix{{{1, 1}, {5, 1}, {5, 3}, {1, 3}, {1, 1}}},
+			},
+			}, want: []matrix.Steric{matrix.PolygonMatrix{{{2, 1}, {1, 1}, {1, 2}, {1, 3}, {5, 3}, {5, 1}, {2, 1}}},
+				matrix.PolygonMatrix{{{1, 1}, {5, 1}, {5, 3}, {1, 3}, {1, 1}}},
+			},
+			wantErr: false},
+	}
+	for _, tt := range tests {
+		if !geoos.GeoosTestTag && tt.name != "poly poly03" {
+			continue
+		}
+		t.Run(tt.name, func(t *testing.T) {
+			p := &PolygonOverlay{
+				PointOverlay:  tt.fields.PointOverlay,
+				subjectPlane:  tt.fields.subjectPlane,
+				clippingPlane: tt.fields.clippingPlane,
+			}
+			got, err := p.Union()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("PolygonOverlay.Union() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			isEqual := got.EqualsExact(tt.want[0], 0.000001)
+			if len(tt.want) > 1 {
+				isEqual1 := got.EqualsExact(tt.want[1], 0.000001)
+				isEqual = isEqual || isEqual1
+			}
+
+			if !isEqual {
+				t.Errorf("PolygonOverlay.Union()%v = %v, \nwant %v", tt.name, got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPolygonOverlay_Difference(t *testing.T) {
+	type fields struct {
+		PointOverlay  *overlay.PointOverlay
+		subjectPlane  *overlay.Plane
+		clippingPlane *overlay.Plane
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    matrix.Steric
+		wantErr bool
+	}{
+		{"poly poly", fields{PointOverlay: &overlay.PointOverlay{matrix.PolygonMatrix{{{0, 0}, {10, 0}, {10, 10}, {0, 10}, {0, 0}}},
+			matrix.PolygonMatrix{{{5, 5}, {15, 5}, {15, 15}, {5, 15}, {5, 5}}},
+		}},
+			matrix.PolygonMatrix{{{5, 10}, {0, 10}, {0, 0}, {10, 0}, {10, 5}, {5, 5}, {5, 10}}}, false},
+
+		{"poly poly1", fields{PointOverlay: &overlay.PointOverlay{matrix.PolygonMatrix{{{100, 100}, {100, 101}, {101, 101}, {101, 100}, {100, 100}}},
+			matrix.PolygonMatrix{{{90, 90}, {90, 101}, {101, 101}, {101, 90}, {90, 90}}},
+		}},
+			matrix.PolygonMatrix{}, false},
+
+		{"poly poly2", fields{PointOverlay: &overlay.PointOverlay{
+			matrix.PolygonMatrix{{{90, 90}, {90, 101}, {101, 101}, {101, 90}, {90, 90}}},
+			matrix.PolygonMatrix{{{100, 100}, {100, 101}, {101, 101}, {101, 100}, {100, 100}}},
+		}},
+			matrix.PolygonMatrix{{{100, 101}, {90, 101}, {90, 90}, {101, 90}, {101, 100}, {100, 100}, {100, 101}}}, false},
+		{"poly poly3", fields{PointOverlay: &overlay.PointOverlay{
+			matrix.PolygonMatrix{{{1, 1}, {5, 1}, {5, 5}, {1, 5}, {1, 1}}},
+			matrix.PolygonMatrix{{{2, 2}, {3, 2}, {3, 3}, {2, 3}, {2, 2}}},
+		}},
+			matrix.PolygonMatrix{{{1, 1}, {5, 1}, {5, 5}, {1, 5}, {1, 1}}, {{2, 2}, {3, 2}, {3, 3}, {2, 3}, {2, 2}}}, false},
+	}
+	for _, tt := range tests {
+		if !geoos.GeoosTestTag && tt.name != "poly poly2" {
+			continue
+		}
+		t.Run(tt.name, func(t *testing.T) {
+			p := &PolygonOverlay{
+				PointOverlay:  tt.fields.PointOverlay,
+				subjectPlane:  tt.fields.subjectPlane,
+				clippingPlane: tt.fields.clippingPlane,
+			}
+			got, err := p.Difference()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("PolygonOverlay.Difference() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("PolygonOverlay.Difference()%v = %v, \nwant %v", tt.name, got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPolygonOverlay_SymDifference(t *testing.T) {
+	type fields struct {
+		PointOverlay  *overlay.PointOverlay
+		subjectPlane  *overlay.Plane
+		clippingPlane *overlay.Plane
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    matrix.Steric
+		wantErr bool
+	}{
+		{"poly poly", fields{PointOverlay: &overlay.PointOverlay{matrix.PolygonMatrix{{{0, 0}, {10, 0}, {10, 10}, {0, 10}, {0, 0}}},
+			matrix.PolygonMatrix{{{5, 5}, {15, 5}, {15, 15}, {5, 15}, {5, 5}}},
+		}},
+			matrix.Collection{matrix.PolygonMatrix{{{5, 10}, {0, 10}, {0, 0}, {10, 0}, {10, 5}, {5, 5}, {5, 10}}},
+				matrix.PolygonMatrix{{{10, 5}, {15, 5}, {15, 15}, {5, 15}, {5, 10}, {10, 10}, {10, 5}}}}, false},
+		{"poly poly0", fields{PointOverlay: &overlay.PointOverlay{matrix.PolygonMatrix{{{100, 100}, {100, 101}, {101, 101}, {101, 100}, {100, 100}}},
+			matrix.PolygonMatrix{{{90, 90}, {90, 101}, {101, 101}, {101, 90}, {90, 90}}},
+		}},
+			matrix.Collection{
+				matrix.PolygonMatrix{{{100, 101}, {90, 101}, {90, 90}, {101, 90}, {101, 100}, {100, 100}, {100, 101}}}},
+			false},
+		{"poly poly1", fields{PointOverlay: &overlay.PointOverlay{
+			matrix.PolygonMatrix{{{90, 90}, {90, 101}, {101, 101}, {101, 90}, {90, 90}}},
+			matrix.PolygonMatrix{{{100, 100}, {100, 101}, {101, 101}, {101, 100}, {100, 100}}},
+		}},
+			matrix.Collection{matrix.PolygonMatrix{{{100, 101}, {90, 101}, {90, 90}, {101, 90}, {101, 100}, {100, 100}, {100, 101}}}},
+			false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := &PolygonOverlay{
+				PointOverlay:  tt.fields.PointOverlay,
+				subjectPlane:  tt.fields.subjectPlane,
+				clippingPlane: tt.fields.clippingPlane,
+			}
+			got, err := p.SymDifference()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("PolygonOverlay.SymDifference() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("PolygonOverlay.SymDifference() = %v, \nwant %v", got, tt.want)
+			}
+		})
+	}
+}
